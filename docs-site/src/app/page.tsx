a4e6c468bcf097b7bd30c759389e24224617d902
Michel DeBauge <mdebau@gmail.com>
2024-11-23 15:53:24 -0800

    moving to monorepo

--- docs-site/src/app/page.tsx
+++ docs-site/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import H1 from "@/components/h1";
 import { usePokemon } from "pokeapi-sdk";
 
 export default function Home() {
@@ -13,7 +14,7 @@ export default function Home() {
       <p className="text-xs uppercase font-bold mb-2 mt-2 text-rose-400">
         Overview
       </p>
-      <h1 className="text-3xl font-bold">Introduction</h1>
+      <H1>Introduction</H1>
       <p className="mt-4">
         This is a simple TypeScript client for fetching Pokemon data from the
         PokeAPI. It&apos;s designed to be used in Next.js projects.
@@ -21,6 +22,8 @@ export default function Home() {
       {pokemonLoading && <p>Loading...</p>}
       {pokemonError && <p>Error: {pokemonError.message}</p>}
       {pokemon && <p>Loaded Pokemon: {pokemon.name}</p>}
+      <H1>Installation</H1>
+      <H1>Quick Start</H1>
     </div>
   );
 }
